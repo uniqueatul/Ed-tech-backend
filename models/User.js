002e22f046d76068ac5e2cbdaaaf2580374a76cf
Atul kumar singh <atulkumarsingh958@gmail.com>
2025-09-25 02:25:48 +0530

    first commit

--- models/User.js
+++ models/User.js
@@ -1,2 +1,62 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+
+    firstname: {
+        type: String,
+        required: true ,
+        trim: true,
+        
+    },
+
+    lastName: {
+        type: String,
+        required: true ,
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: true ,
+        trim: true,
+        
+    },
+    password: {
+        type: String,
+        required: true ,
+        
+    },
+    accountType: {
+        type: String,
+        enum: ['Admin', 'student' , 'Instructor'],
+        required: true
+    },
+    
+    additionalDetails: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "profile",
+
+    },
+
+    course: [
+        { type: mongoose.Schema.Types.ObjectId,
+        ref: "course",}
+     ] ,
+
+    Image: {
+        type: String,
+        required: true,
+    },
+
+    courseProgress:  [
+        {
+        type:mongoose.Schema.types.ObjectId,
+        ref: "courseProgress",
+    }
+
+    ],
+
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ models/profile.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const profileSchema = new mongoose.Schema({
+
+   
+    
+
+});
+
+module.exports = mongoose.model('profile', userSchema);
